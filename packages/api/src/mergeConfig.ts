04ae1686f455b4ea69efa87ea3505c3e58324a9e
kunal-pebble <kunal.singh@gopebble.com>
2025-05-02 08:56:24 +0530

    removed defaults from config merge

--- packages/api/src/mergeConfig.ts
+++ packages/api/src/mergeConfig.ts
@@ -46,24 +46,6 @@ export const mergeConfig = (ccfConfig: Partial<CCFConfig>) => {
     ON_PREMISE: {
       ..._config.ON_PREMISE,
       ...ccfConfig.ON_PREMISE,
-      SERVER: {
-        ..._config.ON_PREMISE?.SERVER,
-        ...ccfConfig.ON_PREMISE?.SERVER,
-        CPU_UTILIZATION: ccfConfig.ON_PREMISE?.SERVER?.CPU_UTILIZATION ?? 50,
-        AVERAGE_WATTS: ccfConfig.ON_PREMISE?.SERVER?.AVERAGE_WATTS ?? 0,
-      },
-      LAPTOP: {
-        ..._config.ON_PREMISE?.LAPTOP,
-        ...ccfConfig.ON_PREMISE?.LAPTOP,
-        CPU_UTILIZATION: ccfConfig.ON_PREMISE?.LAPTOP?.CPU_UTILIZATION ?? 50,
-        AVERAGE_WATTS: ccfConfig.ON_PREMISE?.LAPTOP?.AVERAGE_WATTS ?? 0,
-      },
-      DESKTOP: {
-        ..._config.ON_PREMISE?.DESKTOP,
-        ...ccfConfig.ON_PREMISE?.DESKTOP,
-        CPU_UTILIZATION: ccfConfig.ON_PREMISE?.DESKTOP?.CPU_UTILIZATION ?? 50,
-        AVERAGE_WATTS: ccfConfig.ON_PREMISE?.DESKTOP?.AVERAGE_WATTS ?? 0,
-      },
     },
   }
   return _config
